7190126a1ec34f72c50ca6e49e69060cbb594e71
stlouisn <stlouisn@hotmail.com>
2024-09-27 17:25:23 -0300

    restructured as neoforge only

---
+++ src/main/java/dev/no_night_vision_flickering/NoNightVisionFlickering.java
@@ -0,0 +1,12 @@
+package dev.no_night_vision_flickering;
+
+import dev.no_night_vision_flickering.utils.ModConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("unused")
+public final class NoNightVisionFlickering {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(ModConstants.MOD_ID);
+
+}
---
+++ src/main/java/dev/no_night_vision_flickering/utils/ModConstants.java
@@ -0,0 +1,6 @@
+package dev.no_night_vision_flickering.utils;
+
+public final class ModConstants {
+
+  public static final String MOD_ID = "no_night_vision_flickering";
+}
